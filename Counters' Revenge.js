768b5fce159bd495dc9a28aa216e9dacac46133a
S-e-r-g-i-o <S-e-r-g-i-o@users.noreply.github.com>
2021-02-02 12:10:36 -0800

    Create Counters' Revenge.js

---
+++ Counters' Revenge.js
@@ -0,0 +1,53 @@
+// ==UserScript==
+// @name         Counters' Revenge
+// @namespace    http://tampermonkey.net/
+// @version      0.1       02/02/2021 12:00PM
+// @description 
+// @author       S-e-r-g-i-o
+// @match https://aftlite-portal.amazon.com/bcc/enter-asin
+// @match https://aftlite-na.amazon.com/bcc/enter-asin
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+  // get input for the length of list or array needed.
+  var ASINS = ( prompt ("How many different ASINS are in the bin? Enter 0 to end script."));
+    if(ASINS == 0){
+       return;
+       }
+
+
+    // undefined array of ASINS length, stores ASINS.
+  var asinArray = Array.apply(null, Array(ASINS)).map(function () {})
+
+    // undefined array of ASINS length, stores quantity.
+  var quantityArray = Array.apply(null, Array(ASINS)).map(function () {})
+
+      // populates asinArray with ASINS, quantityArray with asin quantities
+     for(var i = 0; i < ASINS; i++){
+
+          //i+1 to avoid user confusion from 0.
+          asinArray[i] = prompt ("Please type ASIN #" + (i+1) +" out of " + (ASINS));
+
+          quantityArray = prompt ("Please type in the quantity of ASIN " + (asinArray[i]));
+     }
+
+    //controls which asin to output
+    for (var y = 0; y < asinArray.length; y++ ) {
+
+        // controls qty of asinArray[y] ASIN
+        for (var x = 0; x < quantityArray[x]; x++) {
+
+            // site response/load delay
+            setTimeout(function(){
+                document.getElementById('asin').value = asinArray[y];
+                document.querySelector("input[type='submit']").click();
+
+            },1000);
+
+        }
+    }
+return;
+})();
