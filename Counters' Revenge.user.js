ae21be73f0c67eada8253846af8fcf3d71a99406
S-e-r-g-i-o <S-e-r-g-i-o@users.noreply.github.com>
2021-03-01 14:06:57 -0800

    Update Counters' Revenge.user.js

--- Counters' Revenge.user.js
+++ Counters' Revenge.user.js
@@ -1,67 +1,64 @@
 // ==UserScript==
 // @name         Counters' Revenge
 // @namespace    http://tampermonkey.net/
-// @version      0.2       02/02/2021 11:31PM
+// @version      0.02.24.21 - 11:11PM
 // @author       S-e-r-g-i-o
-// @match https://aftlite-portal.amazon.com/bcc/enter-asin
-// @match https://aftlite-na.amazon.com/bcc/enter-asin
-// @downloadURL https://github.com/S-e-r-g-i-o/refactored-broccoli/blob/master/Counters'%20Revenge.js
+// @match        https://aftlite-portal.amazon.com/bcc/enter-asin*
+// @match        https://aftlite-na.amazon.com/bcc/enter-asin*
+// @downloadURL  https://github.com/S-e-r-g-i-o/refactored-broccoli/blob/master/Counters'%20Revenge.js
+// @grant        GM_setValue
+// @grant        GM_getValue
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-  // get input for the length of list or array needed.
-  var ASINS = ( prompt ("How many different ASINS are in the bin? Enter 0 to end script."));
-    if(ASINS == 0){
-       return;
-       }
-
+    // get input for the length of list or array needed.
+    var ASINS = prompt("How many different ASINS are in the bin? Enter n to end script.");
+    if(ASINS == "nbb  "){
+        return;
+    }
 
     // undefined array of ASINS length, stores ASINS.
-  var asinArray = Array.apply(null, Array(ASINS)).map(function () {})
+    var asinArray = Array.apply(null, Array(ASINS)).map(function () {})
 
     // undefined array of ASINS length, stores quantity.
-  var quantityArray = Array.apply(null, Array(ASINS)).map(function () {})
+    var quantityArray = Array.apply(null, Array(ASINS)).map(function () {})
 
-      // populates asinArray with ASINS, quantityArray with asin quantities
-     for(var i = 0; i < ASINS; i++){
+    // populates asinArray with ASINS, quantityArray with asin quantities
+    for(var i = 0; i < ASINS; i++){
 
-          //i+1 to avoid user confusion from 0.
-          asinArray[i] = prompt ("Please type ASIN #" + (i+1) +" out of " + (ASINS));
+        //i+1 to avoid user confusion from 0.
+        asinArray[i] = prompt ("Please type ASIN #" + (i+1) +" out of " + (ASINS));
 
-          quantityArray = prompt ("Please type in the quantity of ASIN " + (asinArray[i]));
-     }
-
+        quantityArray = prompt ("Please type in the quantity of ASIN " + (asinArray[i]));
+    }
+    /*
     //controls which asin to output
     for (var y = 0; y < asinArray.length; y++ ) {
 
-
         // controls qty of asinArray[y] ASIN
         for (var x = 0; x < quantityArray[x]; x++) {
-
-            let request = new XMLHttpRequest();
-            request.open("POST", asinArray[y], true);
-            request.responseType = "document";
-
-            let formData = new FormData(dateForm);
-
-            request.onreadystatechange = function() {
-    
-                let functions = getFunctions(this.responseXML);
-                let times = this.responseXML.querySelectorAll("b");  
-        }              
-                
-    }
-/*            // site response/load delay
             setTimeout(function(){
+
                 document.getElementById('asin').value = asinArray[y];
+
                 document.querySelector("input[type='submit']").click();
 
             },1000);
-
         }
     }
-*/    
+*/
+    var currentData = JSON.Parse(GM_setValue("asinArray") || "[]");
+
+// localStorage.setItem("myasinArray", JSON.stringify(asinArray));
+
+/*
+    var a = [1, 2, 3];
+GM_setValue("key", JSON.stringify(a));
+
+var b = JSON.parse(GM_getValue("key"));
+*/
+
 
 })();
