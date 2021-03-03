2780bb28aa013123d018c97a20eee6f5a52e6174
S-e-r-g-i-o <S-e-r-g-i-o@users.noreply.github.com>
2021-03-02 22:52:16 -0800

    Update Counters' Revenge.user.js

--- Counters' Revenge.user.js
+++ Counters' Revenge.user.js
@@ -1,23 +1,27 @@
 // ==UserScript==
 // @name         Counters' Revenge
 // @namespace    http://tampermonkey.net/
-// @version      0.02.24.21 - 11:11PM
+// @version      0.03.02.21 - 11:00PM
 // @author       S-e-r-g-i-o
 // @match        https://aftlite-portal.amazon.com/bcc/enter-asin*
 // @match        https://aftlite-na.amazon.com/bcc/enter-asin*
 // @downloadURL  https://github.com/S-e-r-g-i-o/refactored-broccoli/blob/master/Counters'%20Revenge.js
 // @grant        GM_setValue
 // @grant        GM_getValue
+// @grant        GM_deleteValue
 // ==/UserScript==
+
 
 (function() {
     'use strict';
 
     // get input for the length of list or array needed.
-    var ASINS = prompt("How many different ASINS are in the bin? Enter n to end script.");
-    if(ASINS == "nbb  "){
+    /*    var ASINS = prompt("How many different ASINS are in the bin? Enter 0 to end script.");
+    if(ASINS == 0){
         return;
     }
+*/
+    var ASINS = 3
 
     // undefined array of ASINS length, stores ASINS.
     var asinArray = Array.apply(null, Array(ASINS)).map(function () {})
@@ -25,6 +29,17 @@
     // undefined array of ASINS length, stores quantity.
     var quantityArray = Array.apply(null, Array(ASINS)).map(function () {})
 
+    asinArray[0] = "ASIN0";
+    asinArray[1] = "ASIN1";
+    asinArray[2] = "ASIN2";
+    asinArray[3] = "ASIN3";
+    quantityArray[0] = "QUANTITY0";
+    quantityArray[1] = "QUANTITY1";
+    quantityArray[2] = "QUANTITY2";
+    quantityArray[3] = "QUANTITY3";
+
+
+    /*
     // populates asinArray with ASINS, quantityArray with asin quantities
     for(var i = 0; i < ASINS; i++){
 
@@ -33,6 +48,8 @@
 
         quantityArray = prompt ("Please type in the quantity of ASIN " + (asinArray[i]));
     }
+
+*/
     /*
     //controls which asin to output
     for (var y = 0; y < asinArray.length; y++ ) {
@@ -49,16 +66,15 @@
         }
     }
 */
-    var currentData = JSON.Parse(GM_setValue("asinArray") || "[]");
+    //GM_getValue
+ //    GM_deleteValue("asinArray");
 
-// localStorage.setItem("myasinArray", JSON.stringify(asinArray));
 
-/*
-    var a = [1, 2, 3];
-GM_setValue("key", JSON.stringify(a));
+    JSON.Parse(GM_setValue("asinArray",asinArray));
+    JSON.Parse(GM_setValue("quantityArray",quantityArray));
 
-var b = JSON.parse(GM_getValue("key"));
-*/
 
+
+    //  var quantityData = JSON.Parse(GM_setValue(quantityArray,"quantityArray") || "[]");
 
 })();
